6885f2ddb8691e4516e87b3665cf08831c69d0ea
marco-pacente <marco.pacente@gmail.com>
2023-09-28 12:29:48 +0200

    Aggiunta somma

---
+++ 26-08-23/Sum/sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    printf("Enter two numbers:\t");
+
+    scanf("%d %d", &firstNumber, &secondNumber);
+
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    printf("%d + %d = %d \n", firstNumber, secondNumber, sumOfTwoNumbers);
+}
