6f62c36b5699be61c08612e55c36049b13c02039
marco-pacente <marco.pacente@gmail.com>
2023-10-04 09:23:34 +0200

    Modifica a un commento

--- 26-08-23/Division/division.c
+++ 26-08-23/Division/division.c
@@ -6,5 +6,5 @@ int main(){
     result = n1/n2;
     remainder = n1%n2;//calcolo del resto della divisione tra n1 e n2
     printf("%d/%d = %d with remainder %d\n", n1, n2, result, remainder);
-    //per ora non provate a dividere per zero. O provateci. Comunque non è che funzioni
+    //per ora non provate a dividere per zero. Oppure provateci. Comunque non è che funzioni
 }
