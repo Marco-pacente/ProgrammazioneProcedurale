40d4128cf366c3743ed31455f57959c883b1f21e
marco-pacente <marco.pacente@gmail.com>
2023-10-05 12:33:27 +0200

    Caraatteri

---
+++ Ottobre/05-10-2023/charset.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("A capo\n");
+    printf("\n");
+    printf("Tabulazione: \t");
+    printf("\n");
+    printf("Tabulazione verticale \v");
+    printf("\n");
+    printf("Form feed\f");
+    printf("\n");
+}
