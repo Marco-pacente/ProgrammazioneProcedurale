9deacc68c24cee6d249e1f7337a6146d044e3dd6
marco-pacente <marco.pacente@gmail.com>
2023-10-10 15:40:31 +0200

    booleano e carattere spiegati

---
+++ Ottobre/10-10-23/bool/boolean.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define BOOL char //così si fa la stessa cosa
+#define TRUE 1
+#define FALSE 0
+int main(){
+    bool booleano = false; //in realtà è un modo per girarci attorno per farlo sembrare più ordinato
+    int veroBooleano = 0; //è la stessa cosa
+}
---
+++ Ottobre/10-10-23/char/carattere.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char c = 'a';
+    printf("c in intero = %d\n", c);
+    printf("c in caratteri = %c\\n", c);
+}
