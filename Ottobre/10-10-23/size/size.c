a8f747c080b8f44c1cda0bc75d7b61b962700b67
marco-pacente <marco.pacente@gmail.com>
2023-10-10 15:01:42 +0200

    committa pls

--- Ottobre/10-10-23/size/size.c
+++ Ottobre/10-10-23/size/size.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){ 
+    char ch = 12; //carattere (1 byte)
+    printf("sizeof char %ld\n", sizeof(ch));
     signed int a = 12; //intero senza segno (4 byte)
-    printf("sizeof int %ld\n", sizeof(a));
+    printf("sizeof signed int %ld\n", sizeof(a));
     int b = -5; //4 byte
     printf("sizeof int %ld\n", sizeof(b));
     short c = 127; //2 byte
-    printf("sizeof int %ld\n", sizeof(c));
+    printf("sizeof short %ld\n", sizeof(c));
     long d = 4000000; //8 byte in teoria
-    printf("sizeof int %ld\n", sizeof(d));
+    printf("sizeof long %ld\n", sizeof(d));
     long long int r; //8 byte
-    printf("sizeof int %ld\n", sizeof(r));
+    printf("sizeof long long int %ld\n", sizeof(r));
 }
