a3521c9bbef4dcf813ea44880755cdf483e43019
marco-pacente <marco.pacente@gmail.com>
2023-10-11 09:27:55 +0200

    Finito esempio bool

--- Ottobre/10-10-23/bool/boolean.c
+++ Ottobre/10-10-23/bool/boolean.c
@@ -4,6 +4,21 @@
 #define TRUE 1
 #define FALSE 0
 int main(){
-    bool booleano = false; //in realtà è un modo per girarci attorno per farlo sembrare più ordinato
-    int veroBooleano = 0; //è la stessa cosa
+    bool booleano = true; //in realtà è un modo per girarci attorno per farlo sembrare più ordinato
+    char veroBooleano = 1; //è la stessa cosa
+    BOOL booleanoDiverso = TRUE;
+    if (booleano)
+    {
+        printf("quello di stdbool funziona\n");
+    }
+    
+    if (veroBooleano)
+    {
+        printf("anche quello nativo di c con i caratteri funziona\n");
+    }
+    
+    if (booleanoDiverso)
+    {
+        printf("e anche quello per farlo più ordinato senza stdbool funziona\n");
+    }
 }
