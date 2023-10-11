8d240e0e1f8c262f031392942c2c10d587ec0b49
marco-pacente <marco.pacente@gmail.com>
2023-10-11 10:49:40 +0200

    limits aggiornato

--- Ottobre/11-10-23/limits/limits.c
+++ Ottobre/11-10-23/limits/limits.c
@@ -2,4 +2,8 @@
 #include <limits.h>
 int main(){
     int a = INT_MAX;
+    printf("%d\n", a);
+    printf("%d", a+1);
+    printf("\n-----------------------\n");
+    printf("%d\n", __INT_MAX__);
 }
