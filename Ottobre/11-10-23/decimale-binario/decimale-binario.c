f78dd907c226866b495265ebdc32d0031ea6819f
marco-pacente <marco.pacente@gmail.com>
2023-10-11 10:36:59 +0200

    spiegazione limits.h

--- Ottobre/11-10-23/decimale-binario/decimale-binario.c
+++ Ottobre/11-10-23/decimale-binario/decimale-binario.c
@@ -4,11 +4,17 @@ int main(){
     char binNumber[8];
     printf("Enter an integer decimal number:");
     scanf("%d", &number);
-    printf("Binary: ");
+    printf("Binary (inverted): ");
     for(int i = 7; i >= 0; i--)
     {
         binNumber[i] =  number%2;
-        //printf("%d", number%2);
+        printf("%d", number%2);
         number = number/2;
     }
+    printf("\n");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%c", binNumber[i]); //vabbè vaffanculo c ci riprovo dopo
+    }
+    
 }
---
+++ Ottobre/11-10-23/limits/limits.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <limits.h>
+int main(){
+    int a = INT_MAX;
+}
