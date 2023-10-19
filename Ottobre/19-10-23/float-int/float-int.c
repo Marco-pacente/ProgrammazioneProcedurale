317e06da0e9aa43c1af3db470cad2c998f5521c2
marco-pacente <marco.pacente@gmail.com>
2023-10-19 12:33:03 +0200

    float-int

---
+++ Ottobre/19-10-23/float-int/float-int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+    int a = 3.9;
+    printf("3.9 in intero = %d\n", a); //stampa 3
+    char b = 296.3;
+    printf("296.3 in char: %d\n", b); //stampa 127
+}
