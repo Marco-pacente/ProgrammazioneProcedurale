e428d7a67b9e2dc681e4af23fca5ee188287284a
marco-pacente <marco.pacente@gmail.com>
2023-10-20 11:38:08 +0200

    Aggiunte espressioni

---
+++ Ottobre/20-10-23/espressioni/expressions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    unsigned int a = 12;
+    unsigned int b = 6;
+    b = b + 5; //assegnazione a b il suo valore attuale + 12
+    if (a > b)
+    {
+        printf("allora praticamentw\n");
+    }
+
+    
+}
