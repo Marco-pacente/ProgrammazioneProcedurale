12da79391079e1b005345d45a2719153a35aea43
marco-pacente <marco.pacente@gmail.com>
2023-10-20 12:08:07 +0200

    lvalue

---
+++ Ottobre/20-10-23/lvalues/lvalue.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    a = 7; //a è un lvalue (left value)
+           //perchè riceve un valore
+           //7 è un rvalue (right value)
+           //perchè dà un valore
+           //a può stare a sinistra dell'uguale;
+           //7 solo a destra (non puoi scrivere 7 = a)
+}
