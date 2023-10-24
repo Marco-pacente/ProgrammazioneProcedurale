3cc28ca17fa661f6b1726d111873aee54c138742
marco-pacente <marco.pacente@gmail.com>
2023-10-24 14:21:23 +0200

    priorità

--- Ottobre/20-10-23/proprietà/propriety.c
+++ Ottobre/20-10-23/proprietà/propriety.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a = 3, b = 6, c = 12;
+    printf("a+b*c = %d\n", a+b*c); 
+    printf("a+(b*c) = %d\n", a+(b*c)); //a+b*c = a+(b*c)
+    //il * ha la priorità
+    printf("a/b/c = %d\n", a/b/c); //si procede da destra a sinistra (a/(b/c))
 }
