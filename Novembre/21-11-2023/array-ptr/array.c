24eb56dbca865dd88156da921d904d8cc7311c2b
marco-pacente <marco.pacente@gmail.com>
2023-11-22 09:52:34 +0100

    array puntatori

--- Novembre/21-11-2023/array-ptr/array.c
+++ Novembre/21-11-2023/array-ptr/array.c
@@ -3,14 +3,13 @@
 int main(){
     int arr[5] = {2, 1, 5, 6, 2};
     int i = 0;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%lld\n", (unsigned long long) &arr[i]);
+    for (i = 0; i < 5; i++){
+        printf("%lld\n", (unsigned long long)&arr[i]);
     }
     int *arrPtr = arr;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d\n", *(arrPtr+i));
+    for (i = 0; i < 5; i++){
+        printf("%d\n", *(arrPtr + i));
     }
+    printf("%d\n", *(arrPtr + i));
     return 0;
 }
