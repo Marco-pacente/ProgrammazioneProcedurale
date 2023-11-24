9efe4aacd34fedc6a9774ac3e11aff95ce8e06f9
marco-pacente <marco.pacente@gmail.com>
2023-11-24 11:41:56 +0100

    realloc

--- Novembre/24-11-2023/realloc/realloc.c
+++ Novembre/24-11-2023/realloc/realloc.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 int main(){
     int * p = malloc(sizeof(int));
-
+    *p = 12;
+    printf("%d\n", *p);
     p = realloc(p, sizeof(short int));
+    printf("%d\n", *p);
+    free(p);    
 }
