a21e428d5d0a7ed50a36293360e32beaa44801b9
marco-pacente <marco.pacente@gmail.com>
2023-11-29 10:11:35 +0100

    struct2

--- Novembre/29-11-2023/struct-autorefer/struct2.c
+++ Novembre/29-11-2023/struct-autorefer/struct2.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 typedef struct Song
 {
     char title[32];
-    char composer[32];
+    char artist[32];
     short duration;
 } song_t;
 
@@ -17,7 +18,21 @@ struct Cell
 };
 
 
+void printSong(song_t song){
+    printf("Song title: %s\n", song.title);
+    printf("Song composer: %s\n", song.artist);
+    printf("Song duration: %d s\n", song.duration);
+}
+
 
 int main(){
     song_t fuoridaltunnel;
+    struct Song *pCanzone;
+    pCanzone = &fuoridaltunnel;
+    strcpy(fuoridaltunnel.title, "fuori dal tunnel");
+    strcpy(fuoridaltunnel.artist, "Caparezza");
+    fuoridaltunnel.duration = 210;
+    printSong(fuoridaltunnel);
+    printf("%s\n", pCanzone -> title); //sono la steessa cosa
+    printf("%s\n", (*pCanzone).title); //però così è più disordinato
 }
