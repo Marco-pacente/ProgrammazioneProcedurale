46132a9a060cae9897db297fa24a04446cb83d19
marco-pacente <marco.pacente@gmail.com>
2023-11-30 12:48:36 +0100

    LETTERALMENTE C INVECE DI c

--- Novembre/30-11-2023/storage/storage.c
+++ Novembre/30-11-2023/storage/storage.c
@@ -3,7 +3,7 @@ int x = 6; //globale quindi statica
 int main(){
     auto int a = 5; //viene allocata all'inizio di main e deallocata alla fine
     //ma ormai è già standard così quindi è deprecato
-    int b = 1; //in ANSI c anche questa si comporta allo stesso modo di a
+    int b = 1; //in ANSI C anche questa si comporta allo stesso modo di a
     register int r = 2; //si specifica di non mettere la variabile in memoria ma direttamente in un registro della cpu
     //cioè è più veloce
     //int *p = &r; e questo non si può fare perchè r non è in memoria 
