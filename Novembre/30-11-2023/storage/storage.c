ea05ac19b423f9f43a47e1899c892eb78780a360
marco-pacente <marco.pacente@gmail.com>
2023-11-30 12:56:53 +0100

    differenza variabile statica e globale

--- Novembre/30-11-2023/storage/storage.c
+++ Novembre/30-11-2023/storage/storage.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int x = 6; //globale quindi statica
+
+int somma(int s){
+    static int somma = 0; //viene dichiarata una volta sola
+    somma = somma + s; //ma mantiene il suo valore
+    //non è la stessa cosa di una variabile globale perchè è accessibile solo da somma()
+    return somma;
+}
+
 int main(){
     auto int a = 5; //viene allocata all'inizio di main e deallocata alla fine
     //ma ormai è già standard così quindi è deprecato
@@ -9,4 +17,9 @@ int main(){
     //int *p = &r; e questo non si può fare perchè r non è in memoria 
     static int s = 5; //viene creata appena il programma parte e deallocata quando il programma finisce
     printf("%d\n", a+b+r+s);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", somma(r));
+    }
+    
 }
