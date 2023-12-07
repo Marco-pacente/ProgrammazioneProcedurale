23b04b4238c65ac80f8c24f4409f55ad13a842d6
Marco-pacente <marco.pacente@gmail.com>
2023-12-07 12:23:17 +0100

    altre cose sul linkage

--- Dicembre/06-12-2023/link/lib.c
+++ Dicembre/06-12-2023/link/lib.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "lib.h"
 
-static int b = 4; //variabile "privata", visibile solo in lib.c
-int a;
-void printHello(void){
-    a = 4;
+
+static int b = 4; //variabile "privata", visibile solo in lib.c //linkage interno
+int a = 3; //linkage esterno (visibile in tutto il file e in altri file)
+//static extern int a; non si può scrivere perchè sarebbe contraddittorio
+
+void printHello(int f){ //esterno f
+    int d = 6; //no linkage (visibile solo in questa funzione)
+    static int e; //no linkage (è un altro tipo di static)
     printf("Hello!\n");
+    extern int b;//stesso b di sopra
 }
